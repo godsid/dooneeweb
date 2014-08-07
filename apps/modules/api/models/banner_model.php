8454dd02173af18bb5670e6703355415f296b71e
Banpot Srihawong <banpot.sr@gmail.com>
2014-08-07 20:14:50 +0700

    API init, Home, Movie

--- apps/modules/api/models/banner_model.php
+++ apps/modules/api/models/banner_model.php
@@ -2,24 +2,34 @@
 
 require_once(APPPATH.'libraries/ADODB_Model.php');
 class Banner_model extends ADODB_model {
-
 	public function __construct(){
 		parent::__construct();
 	}
 
 	public function getBanner($bannerID=false){
 		$sql = "SELECT * 
-				FROM ".$this->table('banner')."
+				FROM ".$this->table('banner')." 
+				WHERE banner_id = ".$bannerID." 
+				AND status = 'ACTIVE' ";
+		return $this->adodb->GetRow($sql);
+	}
+	public function getBanners($page,$limit){
+		$sql = "SELECT * 
+				FROM ".$this->table('banner')." 
 				WHERE status = 'ACTIVE' ";
-
-		if($bannerID){
-			$sql.=" AND banner_id = ".$bannerID;
-			return $this->adodb->GetRow($sql);
+		return $this->adodb->GetAll($sql);
+	}
+	public function rewiteData(&$data){
+		if(isset($data['cover'])){
+			$data['cover'] = static_url($data['cover']);
+			
 		}else{
-			return $this->adodb->query($sql)->GetAll();
+			for($i=0,$j=count($data);$i<$j;$i++){
+				$data[$i]['cover'] = static_url($data[$i]['cover']);
+			}
 		}
 	}
 }
 
-/* End of file user.php */
-/* Location: ./application/controllers/user.php */
+/* End of file banner_model.php */
+/* Location: ./application/model/banner_model.php */
--- apps/modules/api/models/movie_model.php
+++ apps/modules/api/models/movie_model.php
@@ -13,20 +13,49 @@ class Movie_model extends ADODB_model {
 				WHERE movie_id = ".$movieID." 
 				AND status = 'ACTIVE' ";
 				
-		return $this->adodb->query($sql);
+		return $this->adodb->GetRow($sql);
 	}
 
-	public function getMoviews($page,$limit){
+	public function getMovies($page,$limit){
 		$sql ="SELECT * 
 				FROM ".$this->table('movie')."
 				WHERE status = 'ACTIVE' ";
-
+		return $this->fetchPage($sql,$page,$limit);
 	}
 
-	public function getHotMoview(){
-
+	public function getHotMovies($page,$limit){
+		$sql ="SELECT * 
+				FROM ".$this->table('movie')."
+				WHERE is_hot = 'YES' 
+				AND status = 'ACTIVE' ";
+		return $this->fetchPage($sql,$page,$limit);
 	}
 
+	public function getSearchMovie($q,$page,$limit){
+		$sql ="SELECT * 
+				FROM ".$this->table('movie')."
+				WHERE (title LIKE '%".$q."%' OR title_en LIKE '%".$q."%')
+				AND status = 'ACTIVE' ";
+		return $this->fetchPage($sql,$page,$limit);
+	}
+
+	public function getSuggestion($q,$page,$limit){
+		$sql ="SELECT * 
+				FROM ".$this->table('movie')."
+				WHERE (title LIKE '".$q."%' OR title_en LIKE '".$q."%')
+				AND status = 'ACTIVE' ";
+		return $this->fetchPage($sql,$page,$limit);
+	}
+	public function rewiteData(&$data){
+		if(isset($data['cover'])){
+			$data['cover'] = static_url($data['cover']);
+			
+		}else{
+			for($i=0,$j=count($data);$i<$j;$i++){
+				$data[$i]['cover'] = static_url($data[$i]['cover']);
+			}
+		}
+	}
 }
 
 /* End of file user.php */
