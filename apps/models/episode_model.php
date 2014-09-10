f8af9a24afa4ac1db44ffbda734339d89e0891e0
Banpot Srihawong <banpot.sr@gmail.com>
2014-09-10 13:13:54 +0700

    Fix Movie Episode

---
+++ apps/models/episode_model.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once(APPPATH.'libraries/ADODB_Model.php');
+class Episode_model extends ADODB_model {
+
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function getEpisode($episodeID=""){
+		$sql ="SELECT * 
+				FROM ".$this->table('episode')." 
+				WHERE episode_id = '".$episodeID."' 
+				" ;
+		return $this->adodb->GetRow($sql);
+	}
+
+}
+
+
+?>
