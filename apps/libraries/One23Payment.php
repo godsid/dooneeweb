8ef9b5456e88dc917b6b7606e474058382a1e80d
Banpot Srihawong <banpot.sr@gmail.com>
2014-09-22 08:28:34 +0700

    no message

--- apps/libraries/One23Payment.php
+++ apps/libraries/One23Payment.php
@@ -13,58 +13,67 @@ class One23Payment {
 		$this->merchantID = $this->CI->config->item('one23_merchantid');
 		$this->currencyCode = $this->CI->config->item('one23_currencycode');
 		$this->countryCode = $this->CI->config->item('one23_countrycode');
-		$this->secretKey = $this->CI->config->item('one23_secretKey');
+
+
+		$this->apiKey = $this->CI->config->item('one23_apikey');
+		$this->merchantPublicKey = $this->CI->config->item('one23_merchant_publickey');
+		$this->merchantPrivateKey = $this->CI->config->item('one23_merchant_privatekey');
+		$this->merchantPassword  = $this->CI->config->item('one23_merchant_password');
+		$this->serverPublicKey = $this->CI->config->item('one23_server_publickey');
+
 		$this->encryptPath = $this->CI->config->item('one23_encrypt_path');
 		$this->decryptPath = $this->CI->config->item('one23_decrypt_path');
-		$this->privateKey = $this->CI->config->item('one23_privatekey');
 		$this->requestUrl = $this->CI->config->item('one23_requesturl');
 
 		$this->frontrespurl = $this->CI->config->item('one23_frontrespurl');
 		$this->backrespurl = $this->CI->config->item('one23_backrespurl');
 		$this->info = $this->CI->config->item('one23_info');
 
+		$this->inquiryUrl = $this->CI->config->item('one23_inquiryurl');
+
 	}
 
-	public function createForm($messgaeID,$invoiceID,$amount,$description,$items=array(),$payerName="",$payerEmail="",$channelCode = "",$payerMobile=""){
+	public function createForm($messgaeID,$invoiceID,$amount,$description,$items=array(),$payerName="",$payerEmail="",$channelCode = "",$agentCode="",$payerMobile=""){
 
 		$produnctItem = "";
 		foreach($items as $item){
 			$produnctItem.="<PaymentItem id=\"".$item['id']."\" name=\"".$item['name']."\" price=\"".sprintf("%012d",$item['price']*100)."\" />";
 		}
 
-	$msg = "<OneTwoThreeReq>
-			<Version>".$this->version."</Version>
-			<TimeStamp>".date('Y-m-d H:i:s:B')."</TimeStamp>
-			<MessageID>".$messgaeID."</MessageID>
-			<MerchantID>".$this->merchantID."</MerchantID>
-			<InvoiceNo>".sprintf("%012d",$invoiceID)."</InvoiceNo>
-			<Amount>".sprintf("%012d",($amount*100))."</Amount>
-			<CurrencyCode>".$this->currencyCode."</CurrencyCode>
-			<CountryCode>".$this->countryCode."</CountryCode>
-			<ProductDesc>".mb_substr($description,0,255)."</ProductDesc>
-			<PaymentItems>".$produnctItem."</PaymentItems>
-			<PayerName>".mb_substr($payerName,0,100)."</PayerName>
-			<PayerEmail>".mb_substr($payerEmail,0,100)."</PayerEmail>
-			<PayerMobile>".mb_substr($payerMobile,0,20)."</PayerMobile>
-			<MerchantUrl>".$this->frontrespurl.'?invoice='.$invoiceID."</MerchantUrl>
-			<APICallUrl>".$this->backrespurl."</APICallUrl>
-			<PayInSlipInfo>".$this->info."</PayInSlipInfo>
-			".(empty($channelCode)?"":"<ChannelCode>".$channelCode."</ChannelCode>")."
-			<PaymentExpiry>".date('Y-m-d',strtotime("+1 day"))."</PaymentExpiry>
-			<SlipLanguage>TH</SlipLanguage>
-			<UserDefined1></UserDefined1>
-			<UserDefined2></UserDefined2>
-			<UserDefined3></UserDefined3>
-			<UserDefined4></UserDefined4>
-			<UserDefined5></UserDefined5>
-			</OneTwoThreeReq>";
-				//<HashValue>".$this->hash(sprintf("%012d",$invoiceID),sprintf("%012d",($amount*100)))."</HashValue>
+		$msg = "<OneTwoThreeReq>
+				<Version>".$this->version."</Version>
+				<TimeStamp>".date('Y-m-d H:i:s:B')."</TimeStamp>
+				<MessageID>".$messgaeID."</MessageID>
+				<MerchantID>".$this->merchantID."</MerchantID>
+				<InvoiceNo>".sprintf("%012d",$invoiceID)."</InvoiceNo>
+				<Amount>".sprintf("%012d",($amount*100))."</Amount>
+				<CurrencyCode>".$this->currencyCode."</CurrencyCode>
+				<CountryCode>".$this->countryCode."</CountryCode>
+				<ProductDesc>".mb_substr($description,0,255)."</ProductDesc>
+				<PaymentItems>".$produnctItem."</PaymentItems>
+				<PayerName>".mb_substr($payerName,0,100)."</PayerName>
+				<PayerEmail>".mb_substr($payerEmail,0,100)."</PayerEmail>
+				<PayerMobile>".mb_substr($payerMobile,0,20)."</PayerMobile>
+				<MerchantUrl>".$this->frontrespurl.'?invoice='.$invoiceID."</MerchantUrl>
+				<APICallUrl>".$this->backrespurl."</APICallUrl>
+				<PayInSlipInfo>".$this->info."</PayInSlipInfo>
+				<AgentCode>".$agentCode."</AgentCode>
+				<ChannelCode>".$channelCode."</ChannelCode>
+				<PaymentExpiry>".date('Y-m-d',strtotime("+1 day"))."</PaymentExpiry>
+				<SlipLanguage>TH</SlipLanguage>
+				<UserDefined1></UserDefined1>
+				<UserDefined2></UserDefined2>
+				<UserDefined3></UserDefined3>
+				<UserDefined4></UserDefined4>
+				<UserDefined5></UserDefined5>
+				<HashValue>".$this->hash(sprintf("%012d",$invoiceID),sprintf("%012d",($amount*100)))."</HashValue>
+				</OneTwoThreeReq>";
 				
-				/*
+				/*<AgentCode>".$agentCode."</AgentCode>
+				<ChannelCode>".$channelCode."</ChannelCode>
 					<AgentCode>BBL</AgentCode>
 					<ChannelCode>ATM</ChannelCode>
 				*/
-		
 		$encryptMsg = $this->encrypt($invoiceID,$msg);
 
 		$resp = "<html><head></head><body><form method=post action=\"".$this->requestUrl."\" id=\"sbfrom\">
@@ -73,7 +82,30 @@ class One23Payment {
 				<script type=\"text/javascript\">document.getElementsByTagName(\"input\")[1].click();</script></body></html";
 		return $resp;
 	}
+	public function inquiry($messgaeID,$invoiceID,$amount,$description,$items=array(),$payerName="",$payerEmail="",$agentCode,$channelCode = "",$payerMobile=""){
 
+		$msg="<InquiryReq>
+			<Version>1.1</Version>
+			<TimeStamp>".date('Y-m-d H:i:s:B')."</TimeStamp>
+			<MessageID>".$messgaeID."</MessageID>
+			<MerchantID>".$this->merchantID."</MerchantID>
+			<InvoiceNo>".sprintf("%012d",$invoiceID)."</InvoiceNo>
+			<Amount>".sprintf("%012d",($amount*100))."</Amount>
+			<RefNo1>98187</RefNo1>
+			<HashValue>".$this->hash(sprintf("%012d",$invoiceID),sprintf("%012d",($amount*100)))."</HashValue>
+			<UserDefined1></UserDefined1>
+			<UserDefined2></UserDefined2>
+			<UserDefined3></UserDefined3>
+			<UserDefined4></UserDefined4>
+			<UserDefined5></UserDefined5>
+		</InquiryReq>";
+		$encryptMsg = $this->encrypt($invoiceID,$msg);
+		$resp = "<html><head></head><body><form method=post action=\"".$this->inquiryUrl."\" id=\"sbfrom\">
+				<input type=\"hidden\" name=\"OneTwoThreeReq\" value=\"".$encryptMsg."\">
+				<input type=\"submit\" style=\"display:none\" value=\"Send\" name=\"submit\"> </form>
+				<script type=\"text/javascript\">document.getElementsByTagName(\"input\")[1].click();</script></body></html";
+		return $resp;
+	}
 	public function submitPayload($transactionID,$amout,$currencyCode){
 
 		 $uniqueTransactionCode = "Invoice".time();
@@ -81,10 +113,10 @@ class One23Payment {
 	}
 
 	private function hash($invoice,$amount){
-		return strtoupper(hash_hmac('sha1',$this->merchantID.$invoice.$amount,$this->secretKey,false));
+		return strtoupper(hash_hmac('sha1',$this->merchantID.$invoice.$amount,$this->apiKey,false));
 	}
 	private function encrypt($invoice,$msg){
-		$key = file_get_contents(APPPATH."libraries/one23pay/123Public.pem"); //public key for encrypt. This is 123's public key
+		$key = file_get_contents($this->serverPublicKey); //public key for encrypt. This is 123's public key
 		$filehash = $invoice.'_'.time();
 		$encfile = $this->encryptPath.'enc_'.$filehash;
 		$msgfile = $this->encryptPath.'msg_'.$filehash;
@@ -136,7 +168,9 @@ Content-Transfer-Encoding: base64
 	    	return false;
 	    }
 	    
-	    if (openssl_pkcs7_decrypt($decfile, $msgfile, $this->secretKey, $this->privatekey)) {
+	    $key = file_get_contents($this->serverPublicKey);
+	    $key_private = array(file_get_contents($this->merchantPrivateKey), $this->merchantPassword);
+	    if (openssl_pkcs7_decrypt($decfile, $msgfile, $key, $key_private)) {
 	        return file_get_contents($msgfile);
 	    }else 
 		{
