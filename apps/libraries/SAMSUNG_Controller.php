86cb506c1293889c144e4ea4d57e16a86003e9f2
Banpot Srihawong <banpot.sr@gmail.com>
2014-09-23 17:39:49 +0700

    fix samsung with token

--- apps/libraries/SAMSUNG_Controller.php
+++ apps/libraries/SAMSUNG_Controller.php
@@ -41,4 +41,18 @@ abstract class SAMSUNG_Controller extends CI_Controller
     	$data = array_merge(array('head'=>$this->head),$data);
     	echo json_encode($data);
     }
+    public function videoUrlHash($basepath){
+        $secret = $this->config->item('samsung_video_secret'); // To make the hash more difficult to reproduce.
+        $baseuri = '/shc/hd/MISS_POTTER_HD'; // This is the file to send to the user.
+        //$uri  = "$baseuri/playlist.m3u8"; // This is the file to send to the user. 
+        //$uri    =  $_GET["contenturl"];  //change to your content location ex. "media.imovie.com/movie1.mp4"
+        $e = time() + 6000; // At which point in time the file should expire. time() + x; would be the usual usage.
+        $rawhash=$secret . $basepath . $e . $_SERVER['REMOTE_ADDR'];
+        //echo "<p>" . $rawhash . "</p>";
+        $m = base64_encode(md5($rawhash , true)); // Using binary hashing.
+        $m = strtr($m, '+/', '-_'); // + and / are considered special characters in URLs, see the wikipedia page linked in references.
+        $m = str_replace('=', '', $m); // When used in query parameters the base64 padding character is considered special.
+        //You have 60 seconds to access via this link
+        return array('hash'=>"?m=".$m."&e=".$e,'rawhash'=>$rawhash);
+    }
 }
