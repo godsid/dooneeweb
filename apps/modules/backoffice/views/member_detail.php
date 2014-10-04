c6b1d874124f6693568201c7fab43299e5c7dabb
Banpot Srihawong <banpot.sr@gmail.com>
2014-10-04 22:20:19 +0700

    backoffice member detail package invoice

--- apps/modules/backoffice/views/member_detail.php
+++ apps/modules/backoffice/views/member_detail.php
@@ -29,5 +29,98 @@
 					</div>
 				</div><!--/span-->
 			</div><!--/row-->
+
+			<div class="row-fluid sortable">		
+				<div class="box span12">
+					<div class="box-header well" data-original-title>
+						<h2><i class="icon-user"></i> Package</h2>
+					</div>
+					<div class="box-content">
+						<div class="row-fluid">
+							<table class="table table-striped table-bordered responsive">
+                        <thead>
+                        <tr>
+                        	<th>id</th>
+                            <th>Package</th>
+                            <th>Create</th>
+                            <th>Expire</th>
+                            <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($packages as $package){
+                        	$expire = strtotime($package['expire_date']);
+                         ?>
+                        <tr>
+                            <td><?=$package['package_id']?></td>
+                            <td class="center"><?=$package['title']?></td>
+                            <td class="center"><?=$package['create_date']?></td>
+                            <td class="center">
+
+                            	<?=$package['expire_date'],"<br/>","เหลือ ",floor(($expire-time())/(60*60*24))," วัน";
+                            ?>
+                            </td>
+                            <td class="center"><?php 
+                            	
+                            	if($expire>time()){?>
+                            		<span class="label-success label label-default">Active</span>
+                            	<?php }else{?>
+                            		<span class="label-default label label-warning ">Expire</span>
+                            	<?php }
+                            	?></td>
+                        </tr>
+                        <?php } ?>
+                        
+                        </tbody>
+                    </table>
+						</div>
+					</div>
+				</div><!--/span-->
+			</div><!--/row-->
+
+			<div class="row-fluid sortable">		
+				<div class="box span12">
+					<div class="box-header well" data-original-title>
+						<h2><i class="icon-user"></i> Payment</h2>
+					</div>
+					<div class="box-content">
+						<div class="row-fluid">
+							<table class="table table-striped table-bordered responsive">
+                        <thead>
+                        <tr>
+                            <th>Invoice</th>
+                            <th>Package</th>
+                            <th>Channel</th>
+                            <th>Agent/Card No.</th>
+                            <th>Create</th>
+                            <th>Response</th>
+                            <th>Ref.</th>
+                            <th>Error</th>
+                            <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($invoices as $invoice) { ?>
+                        <tr>
+                            <td><?=$invoice['invoice_id']?></td>
+                            <td class="center"><?=$invoice['title']?></td>
+                            <td class="center"><?=$invoice['channel']?></td>
+                            <td class="center"><?=$invoice['agent']?><?=$invoice['resp_pan']?></td>
+                            <td class="center"><?=$invoice['create_date']?></td>
+                            <td class="center"><?=$invoice['resp_date']?></td>
+                            <td class="center"><?=$invoice['resp_tran_ref']?></td>
+                            <td class="center"><?=$invoice['resp_fail_reason']?></td>
+                            <td class="center">
+                                <span class="label-warning label label-default"><?=$invoice['status']?></span>
+                            </td>
+                        </tr>
+                        <?php }?>
+                        
+                        </tbody>
+                    </table>
+						</div>
+					</div>
+				</div><!--/span-->
+			</div><!--/row-->
     
 <?php include('footer.php'); ?>
