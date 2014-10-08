eaf58b6e56a9e620821ae4d0cf4124b85a2b0cd9
Banpot Srihawong <banpot.sr@gmail.com>
2014-10-08 17:32:52 +0700

    member History

--- apps/models/member_model.php
+++ apps/models/member_model.php
@@ -114,6 +114,24 @@ class Member_model extends ADODB_model {
 		return $this->fetchPage($sql,$page,$limit);
 	}
 
+	public function setMemberHistory($user_id,$movie_id,$episode_id,$last_time){
+		$data = array(
+					'user_id'=>$user_id,
+					'movie_id'=>$movie_id,
+					'episode_id'=>$episode_id,
+					'last_time'=>$last_time
+				);
+		if($this->adodb->GetRow("SELECT * FROM ".$this->table('history')." WHERE user_id='".$user_id."' AND movie_id='".$movie_id."' ")){
+			return $this->adodb->AutoExecute($this->table('history'),array('episode_id'=>$episode_id,'last_time'=>$last_time),'UPDATE',"user_id='".$user_id."' AND movie_id='".$movie_id."'");
+		}else{
+			if($this->adodb->AutoExecute($this->table('history'),$data,'INSERT')){
+				return $this->adodb->Insert_ID();
+			}else{
+				return false;
+			}
+		}
+	}
+
 	public function getMemberFavorites($user_id,$page=1,$limit=30){
 		$sql = "SELECT m.movie_id,m.title,m.title_en,m.cover 
 				FROM ".$this->table('user_favorite','uf')." 
